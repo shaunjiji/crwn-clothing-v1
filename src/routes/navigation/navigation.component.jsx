c209fa67034a04822f65593f94a2d1fa9920dd58
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-09 15:06:30 -0400

    add links in navigation bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1> Navigation Bar </h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
