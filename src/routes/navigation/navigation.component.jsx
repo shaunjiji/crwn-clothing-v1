1f00b468fa65723460a4d5310ec3c20e9f0744c6
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-12 13:49:50 -0400

    create sign in component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,8 @@ import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
+import "./navigation.styles.scss";
+
 const Navigation = () => {
   return (
     <Fragment>
@@ -10,9 +12,12 @@ const Navigation = () => {
         <Link className="logo-container" to="/">
           <CrwnLogo className="logo" />
         </Link>
-        <div className="links-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
