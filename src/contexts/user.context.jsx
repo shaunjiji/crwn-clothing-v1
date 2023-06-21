a030eb684151df518b12f81e5613fb546bbd403e
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-21 16:12:16 -0400

    refactor user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -15,7 +15,7 @@ export const UserProvider = ({ children }) => {
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
+    const unsubscribe = onAuthStateCghangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
