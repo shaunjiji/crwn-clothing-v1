5ec2c2735b7256f1f81f3853c089f9ab90244766
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-23 13:33:36 -0400

    import CartContext into cart dropdown component; render cartItems using cartItem component

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -25,10 +25,12 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
-  const value = { isCartOpen, setIsCartOpen };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
