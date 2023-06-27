88e21fd3ab92b7c80b006995a86e9eed3b156f5f
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-27 11:18:45 -0400

    create function return categoryMap

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,18 +2,12 @@ import { createContext, useState, useEffect } from "react";
 
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import SHOP_DATA from "../shop-data.js";
-
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    addCollectionAndDocuments("categories", SHOP_DATA);
-  }, []);
 
   const value = { products };
 
