d03ce65e40accfc7221b044e1d8dd5c2d6671146
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-29 14:58:59 -0400

    rename title class to category title in category component to avoid clashes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -18,7 +18,7 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className="title">{category.toUpperCase()}</h2>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
       <div className="category-container">
         {products &&
           products.map((product) => (
