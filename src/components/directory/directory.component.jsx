df43e8c48a6d82d39433429b6208610668845f89
Shaun Jiji <shaunjiji123@gmail.com>
2023-06-29 14:21:57 -0400

    rename category-item to directory-item; refactor imports and styling for this component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 
 import "./directory.styles.scss";
 
@@ -8,7 +8,7 @@ const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
